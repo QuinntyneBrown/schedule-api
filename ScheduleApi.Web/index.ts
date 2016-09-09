ac023e09c69875d73a9f1bc1ec625ce7f5f18f07
Quinntyne Brown <quinntynebrown@gmail.com>
2016-09-09 18:17:36 -0400

    works

--- ScheduleApi.Web/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./bin";
-export * from "./lib";
-export * from "./node_modules";
-export * from "./obj";
-export * from "./Properties";
-export * from "./src";
-export * from "./typings";
